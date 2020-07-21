91f6d499442a30052281422dff5814e89404190c
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-07-21 17:57:36 +0100

    add fetch to get data

--- client/client.js
+++ client/client.js
@@ -1,17 +1,21 @@
 const form = document.querySelector('#my-form');
+const results = document.querySelector('#results');
 form.addEventListener('submit', getRequest)
 
-async function getRequest(e) {
-    
+function getRequest(e) {
     e.preventDefault();
     let search = e.target.searchBar.value;
 
-    const config = {
-        method: 'GET',
-        url: `https://www.googleapis.com/customsearch/v1?key=AIzaSyCXjUAD4K9aY1FkMPWZkLx2uXEQuoDFVn0&cx=007675054066069573056:uzm9atbkkps&q=${search}&num=10`
-    }
+    let res = fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCXjUAD4K9aY1FkMPWZkLx2uXEQuoDFVn0&cx=007675054066069573056:uzm9atbkkps&q=${search}&num=10`)//, options)
+      .then(r => r.json())
+      .then(console.log())
+      .then(getInfo)
+      .catch(console.warn);
 
-    console.log(res.data)
+      console.log(res.data);
+};
+
+function getInfo(search) {
+  console.log(search);
+  results.append(JSON.stringify(search));
 }
-
-getRequest();
