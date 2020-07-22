231a0a00695a6e26185d89a7c90d3b88415ffa80
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-07-22 12:59:13 +0100

    add in for loop so all 10 search links show up

--- client/client.js
+++ client/client.js
@@ -1,9 +1,6 @@
 const form = document.querySelector('#my-form');
 const results = document.querySelector('#results')
-const titl = document.querySelector('#firstTitlePage')
-const lnk = document.querySelector('#firstLinkPage')
-const snip = document.querySelector('#firstSnippitPage')
-const img = document.querySelector('#firstImage')
+
 
 form.addEventListener('submit', getRequest);
 
@@ -11,35 +8,31 @@ function getRequest(e) {
 
     e.preventDefault();
     let search = e.target.searchBar.value;
-
-    let res = fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyCXjUAD4K9aY1FkMPWZkLx2uXEQuoDFVn0&cx=007675054066069573056:uzm9atbkkps&q=${search}&num=10`)
+    let res = fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBARGZeFhbu-VGkHidrPQVe8UxPwHSXIY0&cx=007675054066069573056:uzm9atbkkps&q=${search}&num=10`)
         .then(r => r.json())
         .then(displayResults)
         .catch(console.warn)
 }
 
 function displayResults(search) {
-    let titles = search.items[0].title;
-    let links = search.items[0].link;
-    let snippits = search.items[0].snippet;
-    let image = search.items[0].pagemap.cse_image[0].src;;
-    titl.append(titles);
-    lnk.setAttribute("href", links);
-    snip.append(snippits);
-    img.setAttribute("src", image);
+    for (let i = 0; i <=9; i++) {
+        let titles = search.items[i].title;
+        let links = search.items[i].link;
+        let snippits = search.items[i].snippet;
+        // let image = search.items[i].pagemap.cse_image[0].src;
+        results.innerHTML += `<section> <a id=\"link${i}\" href=\"#\"><h3 id=\"title${i}\"></h3></a><p id=\"snippit${i}\" ></p><img id=\"img${i}\"src=\"\" alt=\"\"> </section>`;
+        const titl = document.querySelector(`#title${i}`)
+        const lnk = document.querySelector(`#link${i}`)
+        const snip = document.querySelector(`#snippit${i}`)
+        // const img = document.querySelector(`#img${i}`)
+        titl.append(titles);
+        lnk.setAttribute("href", links);
+        snip.append(snippits);
+        // img.setAttribute("src", image);
+    }
   };
 
 
-  // function displayResults(search) {
-  //     let a = document.createElement('a')
-  //     let link = document.createTextNode(search.items[1].link);
-  //     a.appendChild(link);
-  //     a.title = search.items[0].title;
-  //     a.href = search.items[0].link
-  //     let outcome = document.body.appendChild(a);
-  //     outcome.textContent =  a.title;
-  //     results.append(outcome.textContent)
-  // };
   // function hideNavBar() {
   //     let hide = document.querySelectorAll("nav");
   //     Array.from(hide).forEach(hidden => {
