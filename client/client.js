0584eb31d862ca5f03ca9141f7bf961dbe4aa001
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-07-23 12:46:07 +0100

    add moveLogo fuction and implement safeguard against image from API  being undefined

--- client/client.js
+++ client/client.js
@@ -11,35 +11,56 @@ function getRequest(e) {
     let res = fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyBARGZeFhbu-VGkHidrPQVe8UxPwHSXIY0&cx=007675054066069573056:uzm9atbkkps&q=${search}&num=10`)
         .then(r => r.json())
         .then(displayResults)
+        .then(hideNavBar)
+        .then(moveLogo)
         .catch(console.warn)
 }
 
 function displayResults(search) {
     for (let i = 0; i <=9; i++) {
+        results.innerHTML += `<section> <a id=\"link${i}\" href=\"#\"><h3 id=\"title${i}\"></h3></a><p id=\"snippit${i}\" ></p><img id=\"img${i}\"src=\"\" alt=\"\"> </section>`;
         let titles = search.items[i].title;
         let links = search.items[i].link;
         let snippits = search.items[i].snippet;
-        let image = search.items[i].pagemap.cse_image[0].src;
-        results.innerHTML += `<section> <a id=\"link${i}\" href=\"#\"><h3 id=\"title${i}\"></h3></a><p id=\"snippit${i}\" ></p><img id=\"img${i}\"src=\"\" alt=\"\"> </section>`;
+        if (!!search.items[i].pagemap.cse_image){
+          let image = search.items[i].pagemap.cse_image[0].src;
+          const img = document.querySelector(`#img${i}`)
+          img.setAttribute("src", image);
+        } else {
+          console.log(`no photo for index ${i}`)
+        }
+
         const titl = document.querySelector(`#title${i}`)
         const lnk = document.querySelector(`#link${i}`)
         const snip = document.querySelector(`#snippit${i}`)
-        const img = document.querySelector(`#img${i}`)
+
         titl.append(titles);
         lnk.setAttribute("href", links);
         snip.append(snippits);
-        img.setAttribute("src", image);
     }
   };
 
 
-  // function hideNavBar() {
-  //     let hide = document.querySelectorAll("nav");
-  //     Array.from(hide).forEach(hidden => {
-  //         if (displayResults) {
-  //             hidden.style.visibility = "hidden";
-  //         } else {
-  //             hidden.style.visibility = "visible"
-  //         }
-  //     })
-  // }
+  function hideNavBar() {
+      let hide = document.querySelectorAll("nav");
+      Array.from(hide).forEach(hidden => {
+          if (displayResults) {
+              hidden.style.visibility = "hidden";
+          } else {
+              hidden.style.visibility = "visible"
+          }
+      })
+  }
+
+  function moveLogo() {
+    let logo = document.querySelector("#logo");
+    let searchBar = document.querySelector("#my-form");
+
+    logo.style.position = "absolute";
+    logo.style.top = "10px";
+    logo.style.left = "70px";
+
+    searchBar.style.position = "absolute";
+    searchBar.style.top = "50px";
+    searchBar.style.left = "350px";
+  }
